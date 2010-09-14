84bf7d7547621c952b439b9e2e89b40b2e960c29
Liam Cooke <noreply@liamcooke.com>
2010-09-14 17:12:58 +0100

    Finished frotz wrapper

--- zplay.py
+++ zplay.py
@@ -7,14 +7,18 @@ from collections import defaultdict
 from fnmatch import fnmatch
 
 
-ZCODE_PATH = os.environ.get('ZCODE_PATH', '')
+ZCODE_PATH = os.environ.get('ZCODE_PATH', '') or os.environ.get('INFOCOM_PATH', '')
 VALID_EXT = re.compile(r'^\.z[1-8]$')
 
 normalise = lambda s: ''.join(s.split()).lower()
 
 def run_game(fn):
     full_path = os.path.join(ZCODE_PATH, fn)
-    return subprocess.call(['frotz', '-d', full_path])
+    try:
+        result = subprocess.call(['frotz', '-d', '-q', '-Z', '0', full_path])
+    except KeyboardInterrupt:
+        result = None
+    return result
 
 def find_games(simple=False):
     games = [] if simple else defaultdict(dict)
@@ -35,18 +39,37 @@ def find_games(simple=False):
                 games[author][title] = fn
     return games
 
+def game_prompt(games):
+    games = sorted(games)
+    print 'Games:'
+    for i, game in enumerate(games):
+        print '%6d. %s' % (i + 1, game[0])
+    try:
+        selection = int(raw_input('\nChoose a game:\n>'))
+    except ValueError:
+        return None
+    if 1 <= selection <= len(games):
+        return games[selection - 1]
+    else:
+        return None
+
 def play_game(pattern):
     pattern = '*%s*' % normalise(pattern)
     games = find_games(simple=True)
-    matches = [g for g in games if fnmatch(normalise(g[1]), pattern)]
+    matches = [g for g in games if fnmatch(normalise(os.path.basename(g[1])), pattern)]
     if len(matches) == 1:
         return run_game(matches.pop()[1])
     elif matches:
-        print 'Matching games:'
-        for title, _ in sorted(matches):
-            print '    %s' % title
+        try:
+            game = game_prompt(matches)
+        except KeyboardInterrupt:
+            return
+        if not game:
+            print 'Invalid selection.'
+            return 1
+        run_game(game[1])
     else:
-        print 'No matches found.'
+        print 'No games found.'
         return 1
 
 def list_games():
